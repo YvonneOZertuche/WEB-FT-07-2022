ec256637997385399775632be0e87bd6b2b4dcb1
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-22 10:09:30 -0500

    daily problem

--- lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
+++ lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
@@ -1,6 +1,24 @@
 # Return a new list with each element multiplied by 5
 
-let nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+# nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+# new = []
+
+
+# for i in nums:
+#   new.append(i*5)
+#   print(new)
+  
+
+
 
 
 # Given a list [("name", "Elie"), ("job", "Instructor")], create a dictionary that looks like this {'job': 'Instructor', 'name': 'Elie'} (the order does not matter).
+
+list = [("name", "Elie"), ("job", "Instructor")]
+my_dic = {}
+
+for i in range(len(list)):
+  print(i)
+  print(list[i])
+  print(f"")  
+     
