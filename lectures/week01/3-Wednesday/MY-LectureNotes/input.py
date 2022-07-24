b842ddb5a9a7b9e78cbcf6230cb6b5e285567b35
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-23 20:54:44 -0500

    input.py

--- lectures/week01/3-Wednesday/MY-LectureNotes/input.py
+++ lectures/week01/3-Wednesday/MY-LectureNotes/input.py
@@ -1,24 +1,38 @@
 # create a variable called name. Set the value of name to input received from user.
+name = input("What is your name?  ")
 
 # create a variable called age, set the value to the age of given by the user input.
 
+age = input("How old are you?   ")
+
 # check the type of the age (must use the print function)
+
+print(type(age))
 
 # convert the age value to an integer 
 
+age = int(input("How old are you?   "))
+
 # print the year the user was born (2022 - age)
+
+year = 2022 - age
+print(f"You were born in {year}.")
+
 
 #check the type of the following (you have to use to print statement to see the results)
 
 
 isValid = "true"
+print(type(isValid))
 
 isValid2 = True 
+print(type(isValid2))
 
 myNum = 3 
+print(type(myNum))
 
 myName = "Larry"
-
+print(type(myName))
 
 
 
