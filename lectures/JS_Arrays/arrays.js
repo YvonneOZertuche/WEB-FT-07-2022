285ddabed45370fe5ec18411b742ae61f6ed38bd
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-27 16:41:55 -0500

    update

---
+++ lectures/JS_Arrays/arrays.js
@@ -0,0 +1,11 @@
+//A list of items is called an array.
+// It is common practice to declare arrays with the 'const' keyword.
+
+let car1 = "Saab"
+let car2 = "Volvo"
+let car3 = "BMW"
+
+//Keyword 'new' can create an array and assign values
+
+const cars = new Array("Saab", "Volvo", "BMW");
+console.log(cars)
