b05376da6865d55178f5ab2a1db889641f579356
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-27 16:43:28 -0500

    updating"

--- lectures/week04/3-Wednesday/js-convert-2-arrow-functions/level1.js
+++ lectures/week04/3-Wednesday/js-convert-2-arrow-functions/level1.js
@@ -1,30 +1,30 @@
 
   
-function lvl1exercise4 () {
+const lvl1exercise4 = () => "Hello World"
 // return a string "Hello World!"
 
-}
 
 
 
-
-function lvl1exercise5 () {
+const lvl1exercise5 = () => ['Hello World']
 // return an array containing the string "Hello World!" and the number 4
 
 
+
+
+
+const lvl1exercise6 = () =>{
+  return {
+    key1: "Hello World",
+    key2: 4
+  }
 }
-
-
-
-function lvl1exercise6 () {
 // return an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4
 
-}
 
 
-
-function lvl1exercise7 () {
+const lvl1exercise7 = () => false
 // return a boolean value 'false' 
-}
+
 
 
