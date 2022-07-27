7254d3843a52f6c1f66128224b72cb046e2325a3
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-26 21:49:51 -0500

    practice file

---
+++ lectures/week04/1-Monday/lab-js-loops/practice.js
@@ -0,0 +1,60 @@
+// ingredients = ['milk', 'sugar', 'vanilla extract', 'dough', 'chocolate']
+// // Write a for loop that prints out each of the ingredients above
+
+// for (let i = 0; i  <= ingredients.length-1; i++){
+//   console.log(ingredients[i])
+
+// Given a list [1,2,3,4], print out all the values in the list.
+
+// list  = [1, 2, 3, 4]
+// for (let i = 0; i < list.length; i++){
+//   console.log(list[i])
+// }
+
+// Write a for loop that prints each sport in the list sport_games
+// sport_games = ["football", "hockey", "baseball", "cricket"]
+
+// for(let i = 0; i < sport_games.length; i++){
+//   console.log(sport_games[i])
+// }
+
+// Sum the Numbers
+// arr = [4, 2, 5, 7, 23, 6, 5]
+// let sum = 0
+// for (let i = 0; i < arr.length; i++){
+  
+//   sum = sum + arr[i]
+// }
+// console.log(sum)
+
+// Print the largest number in the list.
+// arr = [4, 2, 5, 7, 23, 6, 5]
+// let larger = 0
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i] > larger){
+//     larger = arr[i]
+//     console.log(larger)
+//   }
+// }
+// console.log(larger)
+
+// Print the smallest number in the list.
+// arr = [4, 2, 5, 7, 23, 6, 5]
+// smallest = arr[0]
+// for(let i = 0; i < arr.length; i++){
+//   if (arr[i] < smallest){
+//     smallest = arr[i]
+//     console.log(smallest)
+//   }
+// }
+// console.log(smallest)
+
+// Print each number in the list that is even
+// arr = [4, 2, 5, 7, 23, 6, 5]
+// let even = []
+// for(let i = 0; i < arr.length; i++){
+//   if (arr[i] % 2 == 0){
+//     console.log(arr[i])
+//   }
+// }
+
