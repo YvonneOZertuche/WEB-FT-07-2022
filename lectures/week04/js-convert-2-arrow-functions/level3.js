5d1e8772e83aba08791c73f0287946b2d7f19fc9
YvonneOZertuche <aymzertuche1@outlook.com>
2022-07-27 11:20:34 -0500

    labs

---
+++ lectures/week04/js-convert-2-arrow-functions/level3.js
@@ -0,0 +1,30 @@
+function lvl3exercise1(){
+    // Create a "hello" and a "world", return the concatenation of the two
+  
+}
+  
+function lvl3exercise2(){
+    // Create a "hello" and a 12, return the concatenation of the two
+
+}
+
+function lvl3exercise3(){
+    // Create a variable that equals 12 and convert it to a string. Return it.
+
+}
+
+function lvl3exercise4(){
+    // Create a "hello world!" string. Return the length of the string
+
+}
+
+function lvl3exercise5(){
+    // Create a "hello world" string. Return the index of the word "world".
+
+}
+
+console.log(lvl3exercise1()); //hello world
+console.log(lvl3exercise2()); //hello12
+console.log(lvl3exercise3()); //"12"
+console.log(lvl3exercise4()); //12
+console.log(lvl3exercise5()); //1
---
+++ lectures/week04/lab-js-callbacks-anon-functions/js/lecture.js
@@ -0,0 +1,5 @@
+let sum  = (n1, n2) =>{
+  return n1 + n2
+}
+let result = sum(4,6)
+console.log(result)
