d2c12d8a96e4fac6532ddff91952794f83054a3a
YvonneOZertuche <aymzertuche1@outlook.com>
2022-08-04 11:09:43 -0500

    lecture

--- lectures/week05/4-Thursday/fetch.js
+++ lectures/week05/4-Thursday/fetch.js
@@ -1,18 +1,22 @@
 const ourFetch = (url) => {
-  let promise = new Promise((resolve, reject) =>{
-    let xhr = newXMLttpRequest();
+    let promise = new Promise((resolve, reject) =>{
+      let xhr = newXMLttpRequest();
 
-    xhr.open('GET', url)
+      xhr.open('GET', url)
 
-    xhr.send()
+      xhr.send()
+    
+
+        xhr.onreadystatechange = () =>{
+
+        if(xhr.readyState == 4 && xhr.status == 200){
+            let data = JSON.parse(xhr.responseText)
+            resolve(data)
+        }
+        else if (xhr.readyState == 4 && xhr.status !==200){
+          reject('error in data')
+        }
+    } 
   })
-
-
-  if(xhr.readyState == 4 && xhr.status == 200){
-      let data = JSON.parse(xhr.responseText)
-      resolve(data)
-  }
-  else{
-    reject('error in data')
-  }
-  }
+      return promise;
+}
