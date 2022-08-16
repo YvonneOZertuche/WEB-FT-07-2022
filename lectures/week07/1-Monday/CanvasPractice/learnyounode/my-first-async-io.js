6e21814bdda156ef28e688a665eeff77c881a02e
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-16 09:54:35 -0500

    practice

--- lectures/week07/1-Monday/CanvasPractice/learnyounode/my-first-async-io.js
+++ lectures/week07/1-Monday/CanvasPractice/learnyounode/my-first-async-io.js
@@ -1,7 +1,9 @@
-const  fs = require('fs')
-const file = process.argv[2]
+// const  fs = require('fs')
+// const file = process.argv[2]
 
-fs.readFile(file, function (err, contents){
-  const lines = contents.toString().split('\n').length - 1
-  console.log(lines)
-})
+// fs.readFile(file, function (err, contents){
+//   const lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
+
+
