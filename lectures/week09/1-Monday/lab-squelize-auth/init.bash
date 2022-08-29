0be64694792e8a43e3595b92438637603254a73e
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-29 13:34:56 -0500

    updating files

---
+++ lectures/week09/1-Monday/lab-squelize-auth/init.bash
@@ -0,0 +1,3 @@
+sequelize model:generate --name roles --attributes name:string
+
+sequelize model:generate --name users --attributes username:string,email:string,password:string,roeleID:integer
--- lectures/week09/1-Monday/lab-squelize-auth/routes/login.js
+++ lectures/week09/1-Monday/lab-squelize-auth/routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.get('/login', (req,res) => {
 
@@ -7,6 +8,15 @@ router.get('/login', (req,res) => {
     res.render('login')
 })
 
+router.post('/login', async (req,res) => {
+
+    //scrape info from header
+    //check if user is in db 
+    //encrypt login password 
+    //compare the encrypted passwords with what is in db 
+    //place the session on the login can persist 
+
+})
 
 
 module.exports = router;
