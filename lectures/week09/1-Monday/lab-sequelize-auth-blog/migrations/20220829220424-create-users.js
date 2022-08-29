7822802461fd373487c7573a9b4940677b50fc05
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-08-29 17:49:02 -0500

    db and tables created

---
+++ lectures/week09/1-Monday/lab-sequelize-auth-blog/migrations/20220829220424-create-users.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      is_published: {
+        type: Sequelize.BOOLEAN,
+        // references: {
+        //   model: 'users',
+        //   key: false
+        // },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ lectures/week09/1-Monday/lab-sequelize-auth-blog/migrations/20220829220744-create-blogs.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      userID: {
+        type: Sequelize.INTEGER
+      },
+      is_published: {
+        type: Sequelize.BOOLEAN,
+        // references: {
+        //   model: 'blogs',
+        //   key: false
+        // },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('blogs');
+  }
+};
