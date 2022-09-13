69eaf19fecd30d89d7192e892099452abaacd61f
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-09-13 09:58:58 -0500

    updating files

---
+++ lectures/week11/1-Monday/MY-LectureNotes/styling_in_react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return <>
+
+    <Navbar brandLink="Hello World" brandText="July 2022 Cohort" brandDescription="Learning React"/>
+    <div className='row'>
+      <div className='col-6 bg-primary'>Hello World</div>
+      <div className='col-3 bg-success'>Column 3</div>
+      <div className='col-3 bg-warning'>Column 3 Warning</div>
+    </div>
+    <h1 id ="h1a">Styling in React</h1>
+
+    <Welcome />
+  </>
+}
+
+export default App
---
+++ lectures/week11/1-Monday/MY-LectureNotes/styling_in_react/src/components/Welcome.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// import '../index.css'
+
+const Welcome = () => {
+  return (
+  <>
+
+    {/* <div style={{backgroundColor: 'blue', fontSize: '30px'}} className="div1">Welcome to our space</div> */}
+
+    {/* <div style={myStyles.h1Style} className="div1">Welcome to our space</div> */}
+
+    <div style={myStyles.aNewStyle} className="div1">Welcome to our space</div>
+    
+  </>
+  )   
+  
+}
+
+const myStyles = {
+  h1Style : {
+    backgroundColor: 'orange',
+    fontSize: '50px'
+  },
+
+  aNewStyle:{
+    backgroundColor: 'teal',
+    fontSize: '30px'
+  }
+}
+export default Welcome
