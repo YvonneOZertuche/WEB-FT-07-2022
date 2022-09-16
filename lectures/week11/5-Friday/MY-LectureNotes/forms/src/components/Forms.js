1acddabac3550ab4562e7b671d0526c8e26408b3
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-09-16 12:18:02 -0500

    Modified files

--- lectures/week11/5-Friday/MY-LectureNotes/forms/src/components/Forms.js
+++ lectures/week11/5-Friday/MY-LectureNotes/forms/src/components/Forms.js
@@ -2,11 +2,25 @@ import React, {useState} from 'react'
 
 const Forms = () => {
 
-  const [textValue, setTextValue] = useState('')
+  const [textValue, setTextValue] = useState('goodbye')
   const [isValid, setIsValid] = useState(false)
   const [selectValue, setSelectValue] = useState("Seattle")
 
-  // const handleSubmit 
+  const handleSubmit = (e) => {
+    
+    e.preventDefault()
+
+    //send data to database
+
+    let dataObj = {
+      textValue,
+      isValid,
+      selectValue
+    }
+
+    console.log('form was submitted', dataObj)
+
+  }
 
 
   return (
@@ -14,24 +28,28 @@ const Forms = () => {
 
     <h2>{textValue}</h2>
 
-    <form>
+    <h2>{isValid ? "true" : "false"}</h2>
+
+    <h2>{selectValue}</h2>
+
+    <form onSubmit={handleSubmit}>
       <input type='text' value={textValue} onChange={e => setTextValue(e.target.value)} />
 
       <input type='checkbox' value={isValid} onChange={e => setIsValid(e.target.checked)} />
 
-      <select value={selectValue} onChange={e => setSelectValue(e.target.value)} />
+      <select value={selectValue} onChange={e => setSelectValue(e.target.value)} >
 
-      {/* <option value="Houston">Houston</option>
+              <option value="New Braunfels">New Braunfels</option>
               <option value="Atlanta">Atlanta</option>
               <option value="Seattle">Seattle</option>
               <option value="Chicago">Chicago</option>
               <option value="NewYork">New York</option>
-              <option value="SanFrancisco">San Francisco</option> */}
+              <option value="SanFrancisco">San Francisco</option>
+      </select>
 
+      <input type = "submit" />
 
     </form>
-
-
 
     </>
   )
