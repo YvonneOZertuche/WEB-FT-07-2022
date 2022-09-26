e7f40fdae8340fbf9c60b461c26d72987d1834db
Veronica Lino <Veronica@digitalcrafts.com>
2022-09-26 09:19:35 -0500

    redux

--- algorithms/flatten.js
+++ algorithms/flatten.js
@@ -5,3 +5,50 @@ const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10]
 flatten(exampleArray) //[1,2,3,4,5,6,7,8,9,10]
 
  */
+
+//[1,2,[3,4, [5,6,7], 8], 9, 10]
+//                         | <==
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// [3,4, [5,6,7], 8]
+//                | <==
+// [3, 4, 5, 6, 7, 8 ] 
+
+
+// [5,6,7] 
+//     7|
+// [5, 6, 7]
+
+const flatten = (arr) => {
+    
+    return arr.reduce((acc, val) =>{
+
+        if(Array.isArray(val)){
+            return acc.concat(flatten(val))
+
+        } 
+        else{
+            acc.push(val)
+            return acc
+        }
+
+    }, [])
+
+}
+
+
+flatten([1,2,[3,4, [5,6,7], 8], 9, 10])
+
+
+// const sum = (arr) => {
+    
+//     return= arr.reduce((acc, val)=>{
+//         reutrn acc + val
+//     },0)
+
+    
+// }
+
+// sum([1, 2, 3, 4])
+// //       |
+// //   1  3   6 10
--- algorithms/productSum.js
+++ algorithms/productSum.js
@@ -11,9 +11,33 @@ Therefore, the product sum of [x, y] is x + y; the product sum of [x, [y, z]] is
 Sample input
 
 array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+                           |
+
 
 Sample Output
 
 12 // calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
 
  */
+
+const productSum = (arr, depth=1) => {
+    
+    return arr.reducer( (acc, val)=>{
+        if(Array.isArray(val)){
+            return acc + (depth + 1) * (productSum(val, depth + 1))
+        }
+        else{
+            return acc + val
+        }
+    }, 0)
+}
+
+//[-13, 8]
+//-5
+
+// [6, [-13, 8], 4]
+//  6 +  (2 + 1) * -5) ==  6 -15 = -9
+
+// 5 + 2 + (1 + 1) * (6)  + 3  + (1 +1)* -9)
+// 
+
