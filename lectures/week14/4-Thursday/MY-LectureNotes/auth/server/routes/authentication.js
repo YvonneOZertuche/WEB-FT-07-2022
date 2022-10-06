245b8f7796b1903bc5652012e319b06266460cea
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-06 11:14:30 -0500

    updated file structure

--- lectures/week14/4-Thursday/MY-LectureNotes/auth/server/routes/authentication.js
+++ lectures/week14/4-Thursday/MY-LectureNotes/auth/server/routes/authentication.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jwt-simple')
+const secrets = require('./secrets')
+
+//this function returns JWT
+//{id, email, password, createdAt, updatedAt}
+
+
+const token = (userRecord) => {
+
+  let timestamp = new Date().getTime()
+  return jwt.encode({sub:userRecord.id, iat:timestamp},)
+
+}
+
+
+
 
 router.get('/', (req, res) => {
 
