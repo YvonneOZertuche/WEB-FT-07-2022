1f26774f207f4b7ecc6d47c10cc51a20f5cfd064
Veronica Lino <Veronica@digitalcrafts.com>
2022-10-06 14:40:28 -0500

    passport

--- lectures/week14/4-Thursday/veros-class-notes-dont-touch/auth/server/routes/authentication.js
+++ lectures/week14/4-Thursday/veros-class-notes-dont-touch/auth/server/routes/authentication.js
@@ -5,9 +5,20 @@ const secrets = require('../secrets');
 const db = require('../models');
 const bcrypt = require('bcryptjs'); //used to encrypt passwords
 
+const passport = require('passport');
+
+//must initialize passport for it work
+router.use(passport.initialize())
+
 router.use(express.urlencoded({extended: false})) // scrape email and pwd from request header 
 router.use(express.json())  //req.body
 
+
+//* cal authenticate method on passport instance 
+//* this is our gatekeeper middleare
+
+let requireLogin = passport.authenticate('local', {session:false})
+let requireJwt = passport.authenticate('jwt', {session:false})
 
 //this function returns a JWT
 // {id, email, password, createdAt, updatedAt}
@@ -79,15 +90,22 @@ router.post('/register', async (req, res)=>{
 
     }
 
-
-
 })
 
 
-router.post('/login', MIDDLEWARE,  (req, res)=>{
+router.post('/login', requireLogin,  (req, res)=>{
 
-    res.send('JWT')
+    //req.user 
+
+    res.json({token: token(req.user)})
 })
+
+
+router.get('/protected', requireJwt, (req, res)=>{
+
+    res.json({isValid: true})
+})
+
 
 
 module.exports = router;
